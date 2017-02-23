bbc4f97851f7a802f282013a20c39f31135cdcd3
hjw <hujiaw_cn@126.com>
2017-02-23 16:03:18 +0800

    add comments to introduce the functions

--- src/main/scala/com/datageek/arvato/generateIdGraph.scala
+++ src/main/scala/com/datageek/arvato/generateIdGraph.scala
@@ -13,7 +13,7 @@ import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.{Row, SQLContext, SparkSession}
 
 object generateIdGraph {
-    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("org").setLevel(Level.ERROR)
     val testMode = 1
     val debugMode = 0
     val firstTypeEdgeWeight = 0
@@ -27,9 +27,12 @@ object generateIdGraph {
         import sqlContext.implicits._
 
         // define variables
+        /**
+          * prepares some variables for testing
+          */
         //val sourceId: VertexId = 2L
         //val myIdType = "CUST_ID"
-        val sourceType = "CUST_ID"
+        val sourceType = "MOBILE"
         //val myIdType = "MOBILE"
         //val myIdType = "OPENID"
         //val myIdType = "EMAIL"
@@ -40,12 +43,12 @@ object generateIdGraph {
         FileUtils.deleteDirectory(new File(outputDir))
         // define weights of different properties of ID
 
-        // ============================================
-        // =========== step 1
-        // =========== Load graph from files
-        // ===== Generate the ID connection Graphs
-        // ============================================
 
+        /**
+          * ********** step 1 **********
+          * Load graph information from csv files
+          * generate the ID connection graph
+          */
         val dataDir = "./src/test/data/"
 
         // ====== Graph node : all ID information
@@ -61,12 +64,13 @@ object generateIdGraph {
                     fields(4), // ID value
                     //fields(5).toInt,
                     fields(6).toDouble *
-                      (1 + math.log(1 + 1 / fields(7).toDouble) / math.log(2))), // ID weight
+                      orderToWgt(fields(7).toInt)),
+                      //(1 + math.log(1 + 1 / fields(7).toDouble) / math.log(2))), // ID weight
                     0) // days difference from now to last update time
                 )
         }
 
-        if (testMode == 1) {
+        if (testMode >= 1) {
             println("********** hjw test info **********")
             println("*** There are " + allId.count() + " nodes.")
         }
@@ -97,7 +101,7 @@ object generateIdGraph {
                 )
         }
 
-        if (testMode == 1) {
+        if (testMode >= 1) {
             println("********** hjw test info **********")
             println("*** There are " + IdPairs1.count() + " connections of type 1.")
             println("*** There are " + IdPairs2.count() + " connections of type 2.")
@@ -107,7 +111,7 @@ object generateIdGraph {
         val graph = Graph(allId, IdPairs)
 
         // ====== output the whole graph
-        if (debugMode == 1) {
+        if (debugMode >= 1) {
             println("********** hjw debug info **********")
             val details = graph.triplets.map(
                 triplet => triplet.srcAttr._1._2 + " from table " + triplet.srcAttr._1._1 + " with values " + triplet.srcAttr._1._4 +
@@ -121,17 +125,17 @@ object generateIdGraph {
         // create the non-directed graph by adding the reverse of the original graph
         var nonDirectedGraph = Graph(graph.vertices, graph.edges.union(graph.reverse.edges))
 
-        if (testMode == 1) {
+        if (testMode >= 1) {
             println("********** hjw test info **********")
             println("*** There are " + nonDirectedGraph.edges.count() + " connections in final graph.")
         }
 
-        // ==============================================
-        // =========== step 2
-        // =========== Update the time information
-        // ==============================================
+        /**
+          * ********** step 2.A **********
+          * Update the time information
+          */
 
-        // generate the sub-graph which omly contains the time information
+        // generate the sub-graph which only contains the time information
         var IdUpdateTime: RDD[(VertexId, Int)] = allIdLine.map {
             line =>
                 val fields = line.split("\t")
@@ -170,7 +174,7 @@ object generateIdGraph {
             case (_, oldDate, newDate) => math.min(oldDate, newDate.toInt)
         }
 
-        if (testMode == 1) {
+        if (testMode >= 1) {
             println("********** hjw test info **********")
             println("*** There are " + IdUpdateTime.count() + " vertices counted.")
             updateTimeGraph.vertices.repartition(1).sortBy(vertex => vertex._1).saveAsTextFile(outputDir + "/timeGraph2/")
@@ -214,19 +218,27 @@ object generateIdGraph {
     */
 
 
-        //List(sourceIDList)
-
-        //diffValues.repartition(1).sortBy(vertex => vertex._2).saveAsTextFile(outputDir + "/test/")
-
-        var sourceId: Long = 0L
+        //var sourceId: Long = 0L
 
         //for (sourceId <- sourceIDList) {
+        /**
+          * For a given source Vertex,
+          *     we run step 2.B, 2.C, 2.D to define all vertices' weight
+          * @param sourceId : source vertex ID
+          * TODO: change name the fucntion
+          */
         def hjwTest(sourceId: Long):Unit = {
-            // ===========================================
-            // ========== step 3
-            // ===== shortest path algorithm
-            // ===== count the jump times between tables when join
-            // ===========================================
+            /**
+              * ********** step 2.B **********
+              * shortest path algorithm
+              * count the jump times from one table to another when joining them
+              */
+
+            if (testMode >= 1) {
+                println("=========== hjw test info ===============")
+                println("Finding all connected vertices to Vertex No. " + sourceId.toString)
+                println("=========== hjw info end  ===============")
+            }
 
             // Define a initial graph which has the same structure with the original graph
             // vertices has one attribute at beginning
@@ -268,28 +280,32 @@ object generateIdGraph {
                 println(connectedVertices.collect.mkString("\n"))
             }
 
-            // ========================================
-            // ========== step 4
-            // ========== compute final weight
-            // ========================================
+            /**
+              * ********** step 2.C **********
+              * compute final weight for each vertex
+              */
 
             /**  add the min sum of edge-weights as a new attribute into vertex.attr
               *  filter all vertices whose new attribute < inf
               *  it means these vertices are connected to the
               */
             val connectedVerticesAllInfo = nonDirectedGraph.outerJoinVertices(shortestPathGraph.vertices) {
-                case (_, attr, Some(pathLength)) => ((attr._1._3, attr._1._4, computeWeight(attr._1._5, pathLength, attr._2)), pathLength)
+                case (_, attr, Some(pathLength)) => ((attr._1._3, attr._1._4,
+                  computeWeight(attr._1._5, pathLength, attr._2, T_half = 2500, T_total = 3600)), pathLength)
                 case (_, attr, None) => ((attr._1._3, attr._1._4, attr._1._5), Double.PositiveInfinity)
             }.vertices.filter {
                 case (_, attr) => attr._2 < Double.PositiveInfinity
             }
 
 
-            // ================================================
-            // =========== step 5
-            // =========== sum weights by edge type II
-            // =================================================
+            /**
+              * ********** step 2.D
+              * Sum all vertices' weights with same ID value
+              * i.e. all vertices connected bu edge type II
+              */
 
+            // Create a sub-graph contains all ID vertices connected to source id
+            // and type II edges which connect these vertices
             val defaultVertex = (("NULL", "NULL", 0.0), Double.PositiveInfinity)
             var allInfoGraph = Graph(connectedVerticesAllInfo, IdPairs2, defaultVertex).subgraph(
                 vpred = (_, attr) => attr._2 < Double.PositiveInfinity
@@ -297,55 +313,55 @@ object generateIdGraph {
 
             allInfoGraph = Graph(allInfoGraph.vertices, allInfoGraph.reverse.edges.union(allInfoGraph.edges))
 
-            if (testMode == 1) {
+            if (testMode > 1) {
                 println("**************** hjw test info ********************")
                 println(" *** The new graph created for ID vertex " + sourceId)
                 println(" *** there are " + allInfoGraph.numVertices + " vertices and " + allInfoGraph.numEdges + " Edges.")
                 println(allInfoGraph.vertices.collect().mkString("\n"))
             }
 
-            //
+            // Gathering message from all its neighbours (for edge type II)
             val cntedVerticeWgt = allInfoGraph.mapVertices(
                 (_, attr) => attr._1._3
             ).aggregateMessages[Double](
                 triplet => triplet.sendToDst(triplet.srcAttr),
-                (oWeight, rWeight) => oWeight + rWeight
+                _ + _
             )
 
+            // aggregate message : original message + all received message
+            // and get only the first value to avoid duplicating information
             val cntedVerticesFinalInfo = allInfoGraph.outerJoinVertices(cntedVerticeWgt) {
                 case (_, attr, Some(fWgt)) => (attr._1._1, attr._1._2, attr._1._3 + fWgt)
-                case (_, attr, None) => (attr._1._1, attr._1._2, attr._1._3)
+                case (_, attr, None) => attr._1
             }.vertices.map(vertex => vertex._2).map(
                 prop => ((prop._1, prop._2), prop._3)
             ).reduceByKey((a, _) => a)
 
-            if (testMode == 1) {
+            if (testMode > 0) {
                 cntedVerticesFinalInfo.repartition(1)
                   .sortBy(vertex => vertex._1).saveAsTextFile(outputDir + "/allInfo_" + sourceId.toString + "/")
             }
         }
 
-
-        // ================================================
-        // ========== generate the source ID list
-        // =================================================
+        /**
+          * ********** step 3 **********
+          * Get all Id vertices whose type == source ID type.
+          * Generate a list and find all information of
+          * vertices connected to source vertex.
+          * Run iterations for all source vertex.
+          */
         val diffValues = allId.filter(vertex => vertex._2._1._3 == sourceType)
           .map(vertex => (vertex._2._1._4, vertex._1))
           .reduceByKey((a,b) => a).map(_._2)
 
-        var sourceIDList:Array[Long] = Array()
+        val numSrcId = diffValues.count().toInt
+        val sourceIDList:Array[Long] = diffValues.take(numSrcId)
 
-        //hjwTest(4L)
-
-        diffValues.saveAsTextFile(outputDir + "test")
-
+        // Run iterations
+        // TODO: change name of function hjwTest
         var i: Int = 0
-        for (i <- 1 to diffValues.count().toInt) {
-            println("******* " + i + " ***** " + diffValues.take(i)(0) + " ******")
-            //hjwTest(diffValues.take(i)(0))
-        }
+        for (i <- sourceIDList) hjwTest(i)
 
-        //println("*** there are " + sourceIDList.length + " elements in list.")
 
         /*
     val aa = connectedVerticesAllInfo.map{
@@ -356,7 +372,7 @@ object generateIdGraph {
       case (_, attr) => (attr._1._2, attr._1._3)
     }.reduceByKey(_ + _)
 
-    if (testMode == 1){
+    if (testMode > 0){
       println("********** hjw debug info **********")
       println("*** There are " + connectedVerticesAllInfo .count() + " vertices connected to vertex ID = " + sourceId)
       println(connectedVerticesAllInfo.collect.mkString("\n"))
@@ -375,7 +391,7 @@ object generateIdGraph {
         case (_, attr) => attr._1._1 == myIdType
     }
 
-    if (testMode == 1){
+    if (testMode > 0){
       println("********** hjw test info **********")
       println("*** There are " + connectedVerticesType .count() + " vertices connected to vertex ID = "
         + sourceId + " with type " + myIdType )
@@ -393,17 +409,43 @@ object generateIdGraph {
     */
     }
 
-    /**
-     * This function is used to compute the time delay coefficient by a logarithm model
-     * @param daysDiff: difference in days from now to last update time
-     * @param T_half:   days when this coef reduces to 0.5
-     * @param T_total:  days when this coef reduces to 0
-     * NOTE: in this model we should have
-     *        0.5 * T_total < T_half < T_total
-     */
+    /**
+      * Convert order information into a calculative real number between 0 and 1
+      * deal with the ID's order and source Tables' order
+      * @param order        order number
+      * @param maxOrder     the maximum order(only served for descendant mode),
+      *                    default = 10;
+      * @param isAscendent
+      *                    true(default):   order is smaller => more important
+      *                    false:           order is larger => more important
+      * @return     the weight value based on order, the value is between 0 and 1
+      */
+    def orderToWgt(order: Int, maxOrder:Int = 10, isAscendent: Boolean = true): Double = {
+        if (isAscendent) (1 + math.log(1 + 1 / order.toDouble) / math.log(2)) / 2.0
+        else {
+            (1 + math.log(1 + 1 / (maxOrder - order + 1).toDouble) / math.log(2)) / 2.0
+        }
+    }
+
+
+     /**
+       * This function is used to compute the time delay coefficient by a logarithm model
+       * @param daysDiff: difference in days from now to last update time
+       * @param T_half:   days when this coef reduces to 0.5
+       *               default = 200;
+       * @param T_total:  days when this coef reduces to 0
+       *               default = 360
+       * @param defaultTimeCoef: the default coefficient when the information is missing
+       *               default = 0.5;
+       * @return
+       *         time decay coefficient between 0 and 1
+       * @note
+       *       In this model we should make sure that
+       *        0.5 * T_total < T_half < T_total
+       */
     def timeDecayLog(daysDiff: Int, T_half: Int = 200, T_total: Int = 360,
-                     defaultTimeDecay: Double = 0.5): Double = {
-        if (daysDiff < 0) defaultTimeDecay
+                     defaultTimeCoef: Double = 0.5): Double = {
+        if (daysDiff == Int.MaxValue || daysDiff < 0) defaultTimeCoef
         else if (daysDiff >= T_total) 0.0
         else {
             val gamma: Double = (2 * T_half - T_total).toDouble / ((T_total - T_half) * (T_total - T_half)).toDouble
@@ -413,23 +455,62 @@ object generateIdGraph {
         }
     }
 
+
     // TODO: define exponential time decay model
-    def timeDecayExp(daysDiff: Int, T_half: Int, T_total: Int): Double = {
-        0.0
+    /**
+      * This function is used to compute the time delay coefficient by a exponential model
+      * @param daysDiff: difference in days from now to last update time
+      * @param T_half:   days when this coef reduces to 0.5
+      *               default = 100;
+      * @param T_total:  days when this coef reduces to 0
+      *               default = 360
+      * @param defaultTimeCoef: the default coefficient when the information is missing
+      *               default = 0.5;
+      * @return
+      *         time decay coefficient between 0 and 1
+      * @note
+      *       In this model we should make sure that
+      */
+    def timeDecayExp(daysDiff: Int, T_half: Int = 100 , T_total: Int = 360, defaultTimeCoef: Double = 0.5): Double = {
+        if (daysDiff == Int.MaxValue || daysDiff < 0) defaultTimeCoef
+        else if (daysDiff < 0.5 * T_half) defaultTimeCoef
+        else 0.5
     }
 
-    /*
-   * Compute the final weight of vertices
-   *
-   */
-    def computeWeight(w1: Double, numJumps: Double, updateTime: Int,
-                      timeDecayModel: (Int, Int, Int, Double) => Double = timeDecayLog
-                     ): Double = {
-        val alpha = 0.9
-        // remain information when jumping between tables
-        val result = w1 * math.pow(alpha, numJumps) * timeDecayModel(updateTime, 2500, 3600, 0.5)
 
-        // 4 decimals
+    /**
+      * Compute the final weight coefficent of ID
+      * @param w1            basic weight of ID which defined by source table and ID type
+      * @param numJumps     number of jumps between different tables from source ID to the destination
+      * @param updateTime   number of days from the latest update time to now
+      * @param alpha        Restore information when joining between tables
+      *           default = 0.9;
+      * @param timeDecayModel   choose a model to calculate time decay coefficient
+      *           "log"(default):   logarithm model
+      *           "exp":            exponential model
+      * @param T_half       days when time decay coefficient reduces to 0.5
+      *           default = 200;
+      * @param T_total      days when time decay coefficient reduces to 0
+      *            default = 360;
+      * @param defaultTimeCoef     the default coefficient when the information is missing
+      *            default = 0.5;
+      * @return
+      *            the final weight to the vertex, result keep 4 decimals
+      */
+    def computeWeight(w1: Double, numJumps: Double, updateTime: Int, alpha: Double = 0.9,
+                      timeDecayModel: String = "log",
+                      T_half: Int = 200, T_total:Int = 360, defaultTimeCoef:Double = 0.5
+                     ): Double = {
+        //val alpha = 0.9
+
+        val timeCoef = timeDecayModel match {
+            case "log" => timeDecayLog(updateTime, T_half, T_total, defaultTimeCoef)
+            case "exp" => timeDecayExp(updateTime, T_half, T_total, defaultTimeCoef)
+            case _ => defaultTimeCoef
+        }
+
+        val result = w1 * math.pow(alpha, numJumps) * timeCoef
+        // keep 4 decimals
         (result * 10000).toInt / 10000.0
     }
 
